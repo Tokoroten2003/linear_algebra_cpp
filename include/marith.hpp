d9779b47dd31ea3a926da01f9ee97042290b8870
T_Takebayashi <chikurin1014@gmail.com>
2023-06-19 15:40:01 +0900

    [add] constractors (marith) & accessor (marith)
    On branch main
    Changes to be committed:
    	modified:   include/marith.hpp
    	modified:   test/marith_test.cpp

--- include/marith.hpp
+++ include/marith.hpp
@@ -11,8 +11,39 @@ public:
     Marith();
     Marith(const Marith<T, colN, rowN>& mat);
     Marith(const std::array<std::array<T, rowN>, colN>& arr);
+
+    inline const std::array<std::array<T, rowN>, colN>& getElems() const;
+    inline const std::size_t sizeRow() const;
+    inline const std::size_t sizeCol() const;
+    inline const T& at(std::size_t i, std::size_t j) const;
+    inline T& at(std::size_t i, std::size_t j);
 };
 } // namespace mcalc
 
 template <typename T, std::size_t colN, std::size_t rowN>
 mcalc::Marith<T, colN, rowN>::Marith() : elems() {}
+template <typename T, std::size_t colN, std::size_t rowN>
+mcalc::Marith<T, colN, rowN>::Marith(const Marith<T, colN, rowN>& mat) : elems(mat.getElems()) {}
+template <typename T, std::size_t colN, std::size_t rowN>
+mcalc::Marith<T, colN, rowN>::Marith(const std::array<std::array<T, rowN>, colN>& arr) : elems(arr) {}
+
+template <typename T, std::size_t colN, std::size_t rowN>
+inline const std::array<std::array<T, rowN>, colN>& mcalc::Marith<T, colN, rowN>::getElems() const {
+    return elems;
+}
+template <typename T, std::size_t colN, std::size_t rowN>
+inline const std::size_t mcalc::Marith<T, colN, rowN>::sizeRow() const {
+    return rowN;
+}
+template <typename T, std::size_t colN, std::size_t rowN>
+inline const std::size_t mcalc::Marith<T, colN, rowN>::sizeCol() const {
+    return colN;
+}
+template <typename T, std::size_t colN, std::size_t rowN>
+inline const T& mcalc::Marith<T, colN, rowN>::at(std::size_t i, std::size_t j) const {
+    return elems.at(i).at(j);
+}
+template <typename T, std::size_t colN, std::size_t rowN>
+inline T& mcalc::Marith<T, colN, rowN>::at(std::size_t i, std::size_t j) {
+    return elems.at(i).at(j);
+}
--- test/marith_test.cpp
+++ test/marith_test.cpp
@@ -1,10 +1,40 @@
-#include"marith.hpp"
-#include<gtest/gtest.h>
+#include "marith.hpp"
 
-TEST(marith_int_test, default_constractor) {
+#include <gtest/gtest.h>
+
+constexpr size_t m = 100;
+constexpr size_t n = 100;
+
+namespace {
+TEST(MarithIntConstractorTest, def) {
     try {
-        mcalc::Marith<int, 1, 1> m;
-    } catch(const std::exception& e) {
-        ASSERT_TRUE(false);
+        mcalc::Marith<int, m, n> mat_target;
+    } catch (const std::exception& e) {
+        FAIL();
     }
+}
+TEST(MarithIntConstractorTest, copy) {
+    mcalc::Marith<int, m, n> mat;
+    try {
+        mcalc::Marith<int, m, n> mat_target = mat;
+        ASSERT_NE(&mat, &mat_target);
+        ASSERT_EQ(mat.getElems(), mat_target.getElems());
+    } catch(const std::exception& e) {
+        FAIL();
+    }
+}
+TEST(MarithIntConstractorTest, from_array) {
+    std::array<std::array<int, n>, m> arr;
+    try {
+        mcalc::Marith<int, m, n> mat_target(arr);
+        ASSERT_EQ(arr, mat_target.getElems());
+    } catch(const std::exception& e) {
+        FAIL();
+    }
+}
+} // namespace
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }

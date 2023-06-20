1b941dd33d16bd63d313f80b6f2e50bca380bc79
T_Takebayashi <chikurin1014@gmail.com>
2023-06-20 11:45:21 +0900

    [add] accessors of Marith and tests of them On branch main Changes to be committed: 	modified:   include/marith.hpp 	modified:   test/marith_test.cpp

--- include/marith.hpp
+++ include/marith.hpp
@@ -2,52 +2,63 @@
 #include <type_traits>
 
 namespace mcalc {
-template <typename T, std::size_t colN, std::size_t rowN>
+template <typename T, std::size_t rowN, std::size_t colN>
 class Marith {
     static_assert(std::is_arithmetic<T>::value, "The elements' type must be arithmetic!");
     static_assert(colN != 0 && rowN != 0, "The column size and the row size must not be zero!");
-    std::array<std::array<T, rowN>, colN> elems;
+    std::array<std::array<T, colN>, rowN> elems;
 
 public:
     Marith();
-    Marith(const Marith<T, colN, rowN>& mat);
-    Marith(const Marith<T, colN, rowN>&& mat);
-    Marith(const std::array<std::array<T, rowN>, colN>&& arr);
+    Marith(const Marith<T, rowN, colN>& mat);
+    Marith(const Marith<T, rowN, colN>&& mat);
+    Marith(const std::array<std::array<T, colN>, rowN>&& arr);
 
-    inline const std::array<std::array<T, rowN>, colN>& getElems() const;
+    inline const std::array<std::array<T, colN>, rowN>& getElems() const;
     inline const std::size_t sizeRow() const;
     inline const std::size_t sizeCol() const;
-    inline const T& at(std::size_t i, std::size_t j) const;
+    inline const T& at_const(std::size_t i, std::size_t j) const;
     inline T& at(std::size_t i, std::size_t j);
+    inline const std::array<T, colN>& at_const(std::size_t i) const;
+    inline std::array<T, colN>& at(std::size_t i);
 };
 } // namespace mcalc
 
-template <typename T, std::size_t colN, std::size_t rowN>
-mcalc::Marith<T, colN, rowN>::Marith() : elems() {}
-template <typename T, std::size_t colN, std::size_t rowN>
-mcalc::Marith<T, colN, rowN>::Marith(const Marith<T, colN, rowN>& mat) : elems(mat.getElems()) {}
-template <typename T, std::size_t colN, std::size_t rowN>
-mcalc::Marith<T, colN, rowN>::Marith(const Marith<T, colN, rowN>&& mat) : elems(mat.getElems()) {}
-template <typename T, std::size_t colN, std::size_t rowN>
-mcalc::Marith<T, colN, rowN>::Marith(const std::array<std::array<T, rowN>, colN>&& arr) : elems(arr) {}
+template <typename T, std::size_t rowN, std::size_t colN>
+mcalc::Marith<T, rowN, colN>::Marith() : elems() {}
+template <typename T, std::size_t rowN, std::size_t colN>
+mcalc::Marith<T, rowN, colN>::Marith(const Marith<T, rowN, colN>& mat) : elems(mat.getElems()) {}
+template <typename T, std::size_t rowN, std::size_t colN>
+mcalc::Marith<T, rowN, colN>::Marith(const Marith<T, rowN, colN>&& mat) : elems(mat.getElems()) {}
+template <typename T, std::size_t rowN, std::size_t colN>
+mcalc::Marith<T, rowN, colN>::Marith(const std::array<std::array<T, colN>, rowN>&& arr) :
+    elems(arr) {}
 
-template <typename T, std::size_t colN, std::size_t rowN>
-inline const std::array<std::array<T, rowN>, colN>& mcalc::Marith<T, colN, rowN>::getElems() const {
+template <typename T, std::size_t rowN, std::size_t colN>
+inline const std::array<std::array<T, colN>, rowN>& mcalc::Marith<T, rowN, colN>::getElems() const {
     return elems;
 }
-template <typename T, std::size_t colN, std::size_t rowN>
-inline const std::size_t mcalc::Marith<T, colN, rowN>::sizeRow() const {
+template <typename T, std::size_t rowN, std::size_t colN>
+inline const std::size_t mcalc::Marith<T, rowN, colN>::sizeRow() const {
     return rowN;
 }
-template <typename T, std::size_t colN, std::size_t rowN>
-inline const std::size_t mcalc::Marith<T, colN, rowN>::sizeCol() const {
+template <typename T, std::size_t rowN, std::size_t colN>
+inline const std::size_t mcalc::Marith<T, rowN, colN>::sizeCol() const {
     return colN;
 }
-template <typename T, std::size_t colN, std::size_t rowN>
-inline const T& mcalc::Marith<T, colN, rowN>::at(std::size_t i, std::size_t j) const {
+template <typename T, std::size_t rowN, std::size_t colN>
+inline const T& mcalc::Marith<T, rowN, colN>::at_const(std::size_t i, std::size_t j) const {
     return elems.at(i).at(j);
 }
-template <typename T, std::size_t colN, std::size_t rowN>
-inline T& mcalc::Marith<T, colN, rowN>::at(std::size_t i, std::size_t j) {
+template <typename T, std::size_t rowN, std::size_t colN>
+inline T& mcalc::Marith<T, rowN, colN>::at(std::size_t i, std::size_t j) {
     return elems.at(i).at(j);
+}
+template <typename T, std::size_t rowN, std::size_t colN>
+inline const std::array<T, colN>& mcalc::Marith<T, rowN, colN>::at_const(std::size_t i) const {
+    return elems.at(i);
+}
+template <typename T, std::size_t rowN, std::size_t colN>
+inline std::array<T, colN>& mcalc::Marith<T, rowN, colN>::at(std::size_t i) {
+    return elems.at(i);
 }
--- test/marith_test.cpp
+++ test/marith_test.cpp
@@ -2,11 +2,14 @@
 
 #include <gtest/gtest.h>
 
-constexpr size_t m = 100;
-constexpr size_t n = 100;
+constexpr size_t m = 5;
+constexpr size_t n = 10;
 
 namespace {
-TEST(MarithConstructorTest, no_initializer) {
+// -------------------
+// test of constructor
+// -------------------
+TEST(MarithConstructorTest, NoInitializer) {
     try {
         mcalc::Marith<int, m, n> marith_int_target;
         mcalc::Marith<float, m, n> marith_float_target;
@@ -14,7 +17,7 @@ TEST(MarithConstructorTest, no_initializer) {
         FAIL() << "exception thrown: " << e.what() << std::endl;
     }
 }
-TEST(MarithConstructorTest, copy) {
+TEST(MarithConstructorTest, Copy) {
     mcalc::Marith<int, m, n> marith_int;
     mcalc::Marith<float, m, n> marith_float;
     try {
@@ -24,7 +27,7 @@ TEST(MarithConstructorTest, copy) {
         FAIL() << "exception thrown: " << e.what() << std::endl;
     }
 }
-TEST(MarithConstructorTest, move) {
+TEST(MarithConstructorTest, Move) {
     mcalc::Marith<int, m, n> marith_int;
     mcalc::Marith<float, m, n> marith_float;
     try {
@@ -34,7 +37,7 @@ TEST(MarithConstructorTest, move) {
         FAIL() << "exception thrown: " << e.what() << std::endl;
     }
 }
-TEST(MarithConstructorTest, from_array) {
+TEST(MarithConstructorTest, FromArray) {
     std::array<std::array<int, n>, m> arr_int;
     std::array<std::array<float, n>, m> arr_float;
     try {
@@ -42,6 +45,67 @@ TEST(MarithConstructorTest, from_array) {
         mcalc::Marith<float, m, n> marith_float_target(std::move(arr_float));
     } catch (const std::exception& e) {
         FAIL() << "exception thrown: " << e.what() << std::endl;
+    }
+}
+
+// -----------------
+// test of accessors
+// -----------------
+class MarithAccessTest : public ::testing::Test {
+protected:
+    std::array<std::array<int, n>, m> arr_int_f;
+    std::array<std::array<float, n>, m> arr_float_f;
+
+    virtual void SetUp() {
+        for (size_t i = 0; i < m; i++) {
+            for (size_t j = 0; j < n; j++) {
+                arr_int_f.at(i).at(j)   = i + j;
+                arr_float_f.at(i).at(j) = i + j * 0.1;
+            }
+        }
+    }
+};
+
+TEST_F(MarithAccessTest, getElems) {
+    mcalc::Marith<int, m, n> mat_int_target(std::move(arr_int_f));
+    mcalc::Marith<float, m, n> mat_float_target(std::move(arr_float_f));
+    ASSERT_EQ(arr_int_f, mat_int_target.getElems());
+    ASSERT_EQ(arr_float_f, mat_float_target.getElems());
+}
+TEST_F(MarithAccessTest, sizeRow) {
+    mcalc::Marith<int, m, n> mat_int_target(std::move(arr_int_f));
+    mcalc::Marith<float, m, n> mat_float_target(std::move(arr_float_f));
+    ASSERT_EQ(arr_int_f.size(), mat_int_target.sizeRow());
+    ASSERT_EQ(arr_float_f.size(), mat_float_target.sizeRow());
+}
+TEST_F(MarithAccessTest, sizeCol) {
+    mcalc::Marith<int, m, n> mat_int_target(std::move(arr_int_f));
+    mcalc::Marith<float, m, n> mat_float_target(std::move(arr_float_f));
+    ASSERT_EQ(arr_int_f.at(0).size(), mat_int_target.sizeCol());
+    ASSERT_EQ(arr_float_f.at(0).size(), mat_float_target.sizeCol());
+}
+TEST_F(MarithAccessTest, at) {
+    mcalc::Marith<int, m, n> mat_int_target(std::move(arr_int_f));
+    mcalc::Marith<float, m, n> mat_float_target(std::move(arr_float_f));
+    for (size_t i = 0; i < m; i++) {
+        EXPECT_EQ(arr_int_f.at(i), mat_int_target.at(i));
+        EXPECT_EQ(arr_float_f.at(i), mat_float_target.at(i));
+        for (size_t j = 0; j < n; j++) {
+            EXPECT_EQ(arr_int_f.at(i).at(j), mat_int_target.at(i, j));
+            EXPECT_EQ(arr_float_f.at(i).at(j), mat_float_target.at(i, j));
+        }
+    }
+}
+TEST_F(MarithAccessTest, at_const) {
+    mcalc::Marith<int, m, n> mat_int_target(std::move(arr_int_f));
+    mcalc::Marith<float, m, n> mat_float_target(std::move(arr_float_f));
+    for (size_t i = 0; i < m; i++) {
+        EXPECT_EQ(arr_int_f.at(i), mat_int_target.at_const(i));
+        EXPECT_EQ(arr_float_f.at(i), mat_float_target.at_const(i));
+        for (size_t j = 0; j < n; j++) {
+            EXPECT_EQ(arr_int_f.at(i).at(j), mat_int_target.at_const(i, j));
+            EXPECT_EQ(arr_float_f.at(i).at(j), mat_float_target.at_const(i, j));
+        }
     }
 }
 } // namespace
